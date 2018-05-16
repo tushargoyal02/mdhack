5baa79d313d0057ae6eac0bdade81703d96988ab
root <root@localhost.localdomain>
2018-05-16 20:57:15 +0530

    reciever side

---
+++ rec.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2
+
+import socket 
+sender_ip="192.168.10.177"
+myport=9999
+#                     IPV4, for udp 
+# only for reciver 
+# for tcp dgram hatake stream in caps
+# below ,ethod wth argument creating a soket  called s 
+s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+# now conenctng ip and port 
+
+s.bind((sender_ip,myport))
