d81b544f74e225d90f1a7b4a88a4a56f61a2f578
Tushar Goyal <tushargoyal19tj@gmail.com>
2019-10-06 00:41:36 +0530

    socket of each IP and Port address

--- sender.py
+++ sender.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python2
 
 import socket 
+import os
 sender_ip="192.168.10.177"
 myport=9999
 #                     IPV4, for udp 
 # only for reciver 
 # for tcp dgram hatake stream in caps
 # below ,ethod wth argument creating a soket  called s 
-s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)  # creating object of the socket of each IP and Port address
 
 # now conenctng ip and port 
 
